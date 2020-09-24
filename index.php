f5ca9389d69a5fb62fbfd4bd795a96b965d73565
BrunoCarvalho <brunocarvalho038@gmail.com>
2020-09-24 09:31:48 -0300

    update allow origin cors

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+$request_headers = apache_request_headers();
+$http_origin = $request_headers['Origin'];
+$allowed_http_origins   = array(
+    "http://crm-church.test/",
+    "https://crmdev.ministrycrm.us/",
+    "https://crmprod.ministrycrm.us/",
+);
+if (in_array($http_origin, $allowed_http_origins)){
+    header("Access-Control-Allow-Origin: " . $http_origin);
+}
 header("Access-Control-Allow-Headers: AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2");
 header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
 
