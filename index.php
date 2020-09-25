48ba899799a75a5ee91c36530498bc3d2a6b77e4
Bruno Carvalho <brunocarvalho038@gmail.com>
2020-09-25 12:10:03 -0300

    update origin

--- index.php
+++ index.php
@@ -17,8 +17,9 @@ if (in_array($origin, $allowed_http_origins)){
     header("Access-Control-Allow-Origin: " . $origin);
 }
 
-header("Access-Control-Allow-Headers: AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2");
-header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+header("Access-Control-Allow-Headers: Authorization,Content-Type,account-id,X-Requested-With,Accept,Origin,X-XSRF-TOKEN");
+
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT, PATCH");
 header('Access-Control-Allow-Credentials: true');
 
 define('PHPFOX_PARENT_DIR', __DIR__ . DIRECTORY_SEPARATOR);
