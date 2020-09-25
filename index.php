e275ec59a6f788a954357b48a128b2c93b8aff24
Bruno Carvalho <brunocarvalho038@gmail.com>
2020-09-25 11:04:21 -0300

    update origin

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
 
-$request_headers = apache_request_headers();
-$http_origin = $request_headers['Origin'];
+if (array_key_exists('HTTP_ORIGIN', $_SERVER)) { 
+    $origin = $_SERVER['HTTP_ORIGIN']; 
+} 
+else if (array_key_exists('HTTP_REFERER', $_SERVER)) { 
+    $origin = $_SERVER['HTTP_REFERER']; 
+} else { 
+    $origin = $_SERVER['REMOTE_ADDR']; 
+}
 $allowed_http_origins   = array(
     "http://crm-church.test",
     "https://crmdev.ministrycrm.us",
     "https://crmprod.ministrycrm.us",
 );
-if (in_array($http_origin, $allowed_http_origins)){
-    header("Access-Control-Allow-Origin: " . $http_origin);
+if (in_array($origin, $allowed_http_origins)){
+    header("Access-Control-Allow-Origin: " . $origin);
 }
+
 header("Access-Control-Allow-Headers: AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2");
 header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
 
